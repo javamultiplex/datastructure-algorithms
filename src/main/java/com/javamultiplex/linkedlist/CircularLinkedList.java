cd6c4bbe65c6b5025323dc4997d83a8f2160101d
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-07-07 14:18:19 +0530

    Added more funtions in circular linked list

--- src/main/java/com/javamultiplex/linkedlist/CircularLinkedList.java
+++ src/main/java/com/javamultiplex/linkedlist/CircularLinkedList.java
@@ -17,6 +17,10 @@ public class CircularLinkedList {
         }
     }
 
+    /**
+     *
+     * @param value
+     */
     public void insertEnd(int value) {
         Node node = new Node(value);
         if (isEmpty()) {
@@ -31,23 +35,123 @@ public class CircularLinkedList {
         }
     }
 
-    public void insertStart(int value){
-        Node node=new Node(value);
-        if(isEmpty()){
-            head=node;
-        }else{
-            Node temp=head;
-            while (temp.next!=head) {
+    /**
+     *
+     * @param value
+     */
+    public void insertStart(int value) {
+        Node node = new Node(value);
+        if (isEmpty()) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != head) {
                 temp = temp.next;
             }
-            node.next=head;
-            temp.next=node;
-            head=node;
+            node.next = head;
+            temp.next = node;
+            head = node;
         }
     }
 
-    public void clear(){
-        head=null;
+    /**
+     *
+     * @param position
+     * @param value
+     */
+    public void insertMiddle(int position, int value){
+        Node node=new Node(value);
+        Node temp1=null,temp2=head;
+        int count=0;
+        while (count<position){
+            count++;
+            temp1=temp2;
+            temp2=temp2.next;
+        }
+        temp1.next=node;
+        node.next=temp2;
+    }
+
+    /**
+     *
+     * @param position
+     * @param value
+     */
+    public void insertAt(int position, int value){
+        if(position==0){
+            insertStart(value);
+        }else if(position==size()){
+            insertEnd(value);
+        }else if(position>0 && position<size()){
+            insertMiddle(position,value);
+        }else{
+            System.out.println("Invalid position.");
+        }
+    }
+
+
+    public void deleteStart() {
+        if (isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            Node temp = head;
+            while (temp.next != head) {
+                temp = temp.next;
+            }
+            head = head.next;
+            temp.next = head;
+        }
+    }
+
+
+    public void deleteEnd() {
+        if (isEmpty()) {
+            System.out.println("List is empty");
+        } else if (head.next == head) {
+            head = null;
+        }else{
+            Node temp1=null, temp2=head;
+            while (temp2.next!=head){
+                temp1=temp2;
+                temp2=temp2.next;
+            }
+            temp1.next=head;
+        }
+    }
+
+    /**
+     *
+     * @param position
+     */
+    public void deleteMiddle(int position){
+        int count=0;
+        Node temp1=null, temp2=head;
+        while (count<position){
+            count++;
+            temp1=temp2;
+            temp2=temp2.next;
+        }
+        temp1.next=temp2.next;
+    }
+
+    /**
+     *
+     * @param position
+     */
+    public void deleteAt(int position){
+        if(position==0){
+            deleteStart();
+        }else if(position==size()-1){
+            deleteEnd();
+        }else if(position>0 && position<size()-1){
+            deleteMiddle(position);
+        }else{
+            System.out.println("Invalid position.");
+        }
+    }
+
+    public void clear() {
+        head = null;
     }
 
     public boolean isEmpty() {
@@ -55,18 +159,22 @@ public class CircularLinkedList {
     }
 
     public void traverse() {
-        Node temp = head;
-        while (temp.next != head) {
-            System.out.print(temp.value + "->");
-            temp = temp.next;
+        if (isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            Node temp = head;
+            while (temp.next != head) {
+                System.out.print(temp.value + "->");
+                temp = temp.next;
+            }
+            System.out.print(temp.value);
         }
-        System.out.print(temp.value);
     }
 
-    public int size(){
-        if(isEmpty()){
+    public int size() {
+        if (isEmpty()) {
             return 0;
-        }else {
+        } else {
             int count = 1;
             Node temp = head;
             while (temp.next != head) {
--- src/test/java/com/javamultiplex/linkedlist/CircularLinkedListTest.java
+++ src/test/java/com/javamultiplex/linkedlist/CircularLinkedListTest.java
@@ -1,8 +1,9 @@
 package com.javamultiplex.linkedlist;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Rohit Agarwal on 2019-07-06
@@ -12,12 +13,12 @@ public class CircularLinkedListTest {
     private CircularLinkedList circularLinkedList;
 
     @Before
-    public void setUp(){
-        circularLinkedList=new CircularLinkedList();
+    public void setUp() {
+        circularLinkedList = new CircularLinkedList();
     }
 
     @Test
-    public void insertAtEnd(){
+    public void insertAtEnd() {
         circularLinkedList.insertEnd(10);
         circularLinkedList.insertEnd(20);
         circularLinkedList.insertEnd(30);
@@ -26,7 +27,7 @@ public class CircularLinkedListTest {
     }
 
     @Test
-    public void insertAtStart(){
+    public void insertAtStart() {
         circularLinkedList.insertStart(10);
         circularLinkedList.insertStart(20);
         circularLinkedList.insertStart(30);
@@ -36,17 +37,101 @@ public class CircularLinkedListTest {
     }
 
     @Test
-    public void clear(){
+    public void clear() {
         circularLinkedList.insertStart(10);
         circularLinkedList.insertStart(20);
         circularLinkedList.insertStart(30);
         circularLinkedList.insertStart(40);
         circularLinkedList.insertEnd(50);
         circularLinkedList.traverse();
-        Assert.assertEquals(5,circularLinkedList.size());
+        assertEquals(5, circularLinkedList.size());
         circularLinkedList.clear();
-        Assert.assertEquals(0,circularLinkedList.size());
+        assertEquals(0, circularLinkedList.size());
+    }
 
+    @Test
+    public void deleteStart() {
+        circularLinkedList.insertStart(10);
+        circularLinkedList.insertStart(20);
+        circularLinkedList.insertStart(30);
+        circularLinkedList.insertStart(40);
+        circularLinkedList.traverse();
+        assertEquals(4, circularLinkedList.size());
+        System.out.println();
+        circularLinkedList.deleteStart();
+        circularLinkedList.traverse();
+        assertEquals(3, circularLinkedList.size());
+        System.out.println();
+        circularLinkedList.deleteStart();
+        circularLinkedList.traverse();
+        assertEquals(2, circularLinkedList.size());
+    }
 
+    @Test
+    public void deleteEnd() {
+        circularLinkedList.insertEnd(10);
+        circularLinkedList.insertEnd(20);
+        circularLinkedList.insertStart(30);
+        circularLinkedList.traverse();
+        System.out.println();
+        circularLinkedList.deleteEnd();
+        circularLinkedList.traverse();
+        assertEquals(2, circularLinkedList.size());
+        System.out.println();
+        circularLinkedList.deleteEnd();
+        circularLinkedList.traverse();
+        assertEquals(1, circularLinkedList.size());
+        System.out.println();
+        circularLinkedList.deleteEnd();
+        circularLinkedList.traverse();
+        assertEquals(0, circularLinkedList.size());
+    }
+
+    @Test
+    public void insertAt() {
+        circularLinkedList.insertAt(0, 10);
+        circularLinkedList.insertAt(0, 20);
+        circularLinkedList.insertAt(0, 30);
+        circularLinkedList.insertAt(3, 40);
+        circularLinkedList.insertAt(4, 50);
+        circularLinkedList.traverse();
+        System.out.println();
+        assertEquals(5, circularLinkedList.size());
+        circularLinkedList.insertAt(3, 90);
+        circularLinkedList.traverse();
+        System.out.println();
+        assertEquals(6, circularLinkedList.size());
+        circularLinkedList.insertAt(2, 70);
+        circularLinkedList.traverse();
+        System.out.println();
+        assertEquals(7, circularLinkedList.size());
+        circularLinkedList.insertAt(0, 99);
+        circularLinkedList.traverse();
+        System.out.println();
+        circularLinkedList.insertAt(11,9);
+        circularLinkedList.traverse();
+    }
+
+    @Test
+    public void deleteAt(){
+        circularLinkedList.insertEnd(10);
+        circularLinkedList.insertEnd(20);
+        circularLinkedList.insertEnd(30);
+        circularLinkedList.insertEnd(40);
+        circularLinkedList.insertEnd(50);
+        circularLinkedList.insertEnd(80);
+        circularLinkedList.traverse();
+        System.out.println();
+        circularLinkedList.deleteAt(0);
+        circularLinkedList.traverse();
+        System.out.println();
+        circularLinkedList.deleteAt(3);
+        circularLinkedList.traverse();
+        System.out.println();
+        circularLinkedList.deleteAt(1);
+        circularLinkedList.traverse();
+        System.out.println();
+        circularLinkedList.deleteAt(2);
+        circularLinkedList.traverse();
     }
 }
