da45611f02c2f771321fe6789a8b86bcf9cce244
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-09-30 00:30:39 +0530

    Added Bubble sort algorithm

---
+++ src/main/java/com/javamultiplex/sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package com.javamultiplex.sorting;
+
+import java.util.Arrays;
+
+/**
+ * @author Rohit Agarwal on 2019-09-30
+ * <p>
+ * Time complexity -> Worst Case = O(n^2), Best Case = O(k*n)
+ * where k is number of iterations
+ */
+public class BubbleSort {
+
+    private static void sort(int[] arr, int n) {
+        int swapped;
+        for (int i = 0; i < n; i++) {
+            swapped = 0;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    swapped = 1;
+                }
+            }
+            if (swapped == 0) {
+                break;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 50, 40};
+        int n = 5;
+        sort(arr, n);
+        System.out.println(Arrays.toString(arr));
+    }
+}
