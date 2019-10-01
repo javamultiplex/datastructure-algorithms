bd7d500b9d654c683de869ec2f7f3a2b7c2ea9f5
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-10-02 00:51:37 +0530

    Modified counting sort

--- src/main/java/com/javamultiplex/sorting/CountingSort.java
+++ src/main/java/com/javamultiplex/sorting/CountingSort.java
@@ -11,42 +11,57 @@ import java.util.Arrays;
 public class CountingSort {
 
     public static void sort(int[] arr, int n) {
+        int k = getMax(arr, n);
+
+        int[] count = new int[k + 1];
+
+        int[] sorted = new int[n];
+
+        //O(K)
+        for (int i = 0; i <= k; i++) {
+            count[i] = 0;
+        }
+
+        //O(N)
+        for (int i = 0; i < n; i++) {
+            count[arr[i]]++;
+        }
+
+        //O(K)
+        for (int i=1;i<=k;i++){
+            count[i]+=count[i-1];
+        }
+
+        //O(N)
+        for (int i=n-1;i>=0;i--){
+            sorted[--count[arr[i]]]=arr[i];
+        }
+
+        //O(N)
+        for (int i=0;i<n;i++){
+            arr[i]=sorted[i];
+        }
+
+    }
+
+    private static int getMax(int[] arr, int n) {
         int max = arr[0];
         for (int i = 1; i < n; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
         }
-        int k = max;
-
-        int[] aux = new int[k + 1];
-        int[] sorted = new int[n];
-        for (int i = 0; i <= k; i++) {
-            aux[i] = 0;
-        }
-
-        for (int i = 0; i < n; i++) {
-            aux[arr[i]]++;
-        }
-
-        int j = 0;
-        for (int i = 0; i <= k; i++) {
-            int temp = aux[i];
-            while (temp != 0) {
-                sorted[j] = i;
-                temp--;
-                j++;
-            }
-        }
-
-        System.out.println(Arrays.toString(sorted));
-
+        return max;
     }
 
     public static void main(String[] args) {
 
         int[] arr = {5, 2, 9, 5, 2, 3, 5};
         int n=7;
+        System.out.println("Before Sorting ...");
+        System.out.println(Arrays.toString(arr));
         sort(arr,n);
+        System.out.println("After Sorting ...");
+        System.out.println(Arrays.toString(arr));
     }
 }
